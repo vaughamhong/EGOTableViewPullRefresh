0b60209f9c821fbdba8266300fbdd3ab5f735ecb
Vaugham Hong <vaughamhong@Vaughams-MacBook-Pro-2.local>
2013-12-05 13:53:54 -0800

    made changes to support arc

--- EGOTableViewPullRefresh/Classes/View/EGORefreshTableHeaderView.m
+++ EGOTableViewPullRefresh/Classes/View/EGORefreshTableHeaderView.m
@@ -56,7 +56,6 @@
         label.textAlignment = NSTextAlignmentCenter;
 		[self addSubview:label];
 		_lastUpdatedLabel=label;
-		[label release];
 		
 		label = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, frame.size.height - 48.0f, self.frame.size.width, 20.0f)];
 		label.autoresizingMask = UIViewAutoresizingFlexibleWidth;
@@ -68,7 +67,6 @@
 		label.textAlignment = NSTextAlignmentCenter;
 		[self addSubview:label];
 		_statusLabel=label;
-		[label release];
 		
 		CALayer *layer = [CALayer layer];
 		layer.frame = CGRectMake(25.0f, frame.size.height - 65.0f, 30.0f, 55.0f);
@@ -88,7 +86,6 @@
 		view.frame = CGRectMake(25.0f, frame.size.height - 38.0f, 20.0f, 20.0f);
 		[self addSubview:view];
 		_activityView = view;
-		[view release];
 		
 		
 		[self setState:EGOOPullRefreshNormal];
@@ -112,7 +109,7 @@
 		
 		NSDate *date = [_delegate egoRefreshTableHeaderDataSourceLastUpdated:self];
         if(date){
-            NSDateFormatter *dateFormatter = [[[NSDateFormatter alloc] init] autorelease];
+            NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
             [dateFormatter setDateStyle:NSDateFormatterShortStyle];
             [dateFormatter setTimeStyle:NSDateFormatterShortStyle];
             [dateFormatter setTimeZone:[NSTimeZone defaultTimeZone]];
@@ -258,7 +255,6 @@
 	_statusLabel = nil;
 	_arrowImage = nil;
 	_lastUpdatedLabel = nil;
-    [super dealloc];
 }
 
 
